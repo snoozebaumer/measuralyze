7785c855ff0535dc9a04f3122c3d04831cb774fe
Samuel Nussbaumer <samuel.nussbaumer@stud.hslu.ch>
2024-04-02 12:24:44 +0200

    refactor: lower complexity of CellTowerService

--- app/src/main/java/ch/hslu/measuralyze/service/CellTowerService.kt
+++ app/src/main/java/ch/hslu/measuralyze/service/CellTowerService.kt
@@ -10,62 +10,28 @@ import ch.hslu.measuralyze.model.CellTowerInfo
 
 class CellTowerService(private val telephonyManager: TelephonyManager) {
     fun fetchCurrentValues(onCellTowerValuesFetched: (ArrayList<CellTowerInfo>) -> Unit) {
-        val cellInfoList: List<CellInfo>
-        try {
-            cellInfoList = telephonyManager.allCellInfo
+        val cellInfoList = getCellInfoList()
+        val cellTowerInfoList = extractCellTowerInfo(cellInfoList)
+        onCellTowerValuesFetched(cellTowerInfoList)
+    }
+
+    private fun getCellInfoList(): List<CellInfo> {
+        return try {
+            telephonyManager.allCellInfo ?: emptyList()
         } catch (e: SecurityException) {
             Log.e("CellTowerService", "Permission to access cell tower information denied")
-            onCellTowerValuesFetched(ArrayList())
-            return
+            emptyList()
         }
+    }
+
+    private fun extractCellTowerInfo(cellInfoList: List<CellInfo>): ArrayList<CellTowerInfo> {
         val cellTowerInfoList: ArrayList<CellTowerInfo> = ArrayList()
 
         for (cellInfo: CellInfo in cellInfoList) {
-            val cellIdentity = cellInfo.cellIdentity
-            val cellTowerInfo = CellTowerInfo()
-            cellTowerInfo.operator = cellIdentity.operatorAlphaLong.toString()
-
-
-            when (cellInfo) {
-                is CellInfoWcdma -> {
-                    val cellIdentityWcdma = cellInfo.cellIdentity
-                    cellTowerInfo.mcc = cellIdentityWcdma.mccString ?: ""
-                    cellTowerInfo.mnc = cellIdentityWcdma.mncString ?: ""
-                    cellTowerInfo.lac =
-                        if (cellIdentityWcdma.lac == Int.MAX_VALUE) 0 else cellIdentityWcdma.lac
-                    cellTowerInfo.cid =
-                        if (cellIdentityWcdma.cid == Int.MAX_VALUE) 0 else cellIdentityWcdma.cid
-                    val cellSignalStrengthWcdma = cellInfo.cellSignalStrength
-                    cellTowerInfo.rsrp = cellSignalStrengthWcdma.dbm
-                    cellTowerInfo.rsrq = cellSignalStrengthWcdma.asuLevel
-                }
-
-                is CellInfoLte -> {
-                    val cellIdentityLte = cellInfo.cellIdentity
-                    cellTowerInfo.mcc = cellIdentityLte.mccString ?: ""
-                    cellTowerInfo.mnc = cellIdentityLte.mncString ?: ""
-                    cellTowerInfo.lac =
-                        if (cellIdentityLte.tac == Int.MAX_VALUE) 0 else cellIdentityLte.tac
-                    cellTowerInfo.cid =
-                        if (cellIdentityLte.ci == Int.MAX_VALUE) 0 else cellIdentityLte.ci
-                    val cellSignalStrengthLte = cellInfo.cellSignalStrength
-                    cellTowerInfo.rsrp = cellSignalStrengthLte.rsrp
-                    cellTowerInfo.rsrq = cellSignalStrengthLte.rsrq
-                }
-
-                is CellInfoGsm -> {
-                    val cellIdentityGsm = cellInfo.cellIdentity
-                    cellTowerInfo.mcc = cellIdentityGsm.mccString ?: ""
-                    cellTowerInfo.mnc = cellIdentityGsm.mncString ?: ""
-                    cellTowerInfo.lac =
-                        if (cellIdentityGsm.lac == Int.MAX_VALUE) 0 else cellIdentityGsm.lac
-                    cellTowerInfo.cid =
-                        if (cellIdentityGsm.cid == Int.MAX_VALUE) 0 else cellIdentityGsm.cid
-                    val cellSignalStrengthGsm = cellInfo.cellSignalStrength
-                    cellTowerInfo.rsrp = cellSignalStrengthGsm.dbm
-                    cellTowerInfo.rsrq = cellSignalStrengthGsm.asuLevel
-                }
-
+            val cellTowerInfo = when (cellInfo) {
+                is CellInfoWcdma -> extractCellTowerInfoFromWcdma(cellInfo)
+                is CellInfoLte -> extractCellTowerInfoFromLte(cellInfo)
+                is CellInfoGsm -> extractCellTowerInfoFromGsm(cellInfo)
                 else -> {
                     Log.e("CellTowerService", "Unsupported CellInfo type")
                     continue
@@ -73,6 +39,51 @@ class CellTowerService(private val telephonyManager: TelephonyManager) {
             }
             cellTowerInfoList.add(cellTowerInfo)
         }
-        onCellTowerValuesFetched(cellTowerInfoList)
+        return cellTowerInfoList
+    }
+
+    private fun extractCellTowerInfoFromWcdma(cellInfo: CellInfoWcdma): CellTowerInfo {
+        val cellIdentityWcdma = cellInfo.cellIdentity
+        val cellSignalStrengthWcdma = cellInfo.cellSignalStrength
+
+        return CellTowerInfo().apply {
+            operator = cellIdentityWcdma.operatorAlphaLong.toString()
+            mcc = cellIdentityWcdma.mccString ?: ""
+            mnc = cellIdentityWcdma.mncString ?: ""
+            lac = if (cellIdentityWcdma.lac == Int.MAX_VALUE) 0 else cellIdentityWcdma.lac
+            cid = if (cellIdentityWcdma.cid == Int.MAX_VALUE) 0 else cellIdentityWcdma.cid
+            rsrp = cellSignalStrengthWcdma.dbm
+            rsrq = cellSignalStrengthWcdma.asuLevel
+        }
+    }
+
+    private fun extractCellTowerInfoFromLte(cellInfo: CellInfoLte): CellTowerInfo {
+        val cellIdentityLte = cellInfo.cellIdentity
+        val cellSignalStrengthLte = cellInfo.cellSignalStrength
+
+        return CellTowerInfo().apply {
+            operator = cellIdentityLte.operatorAlphaLong.toString()
+            mcc = cellIdentityLte.mccString ?: ""
+            mnc = cellIdentityLte.mncString ?: ""
+            lac = if (cellIdentityLte.tac == Int.MAX_VALUE) 0 else cellIdentityLte.tac
+            cid = if (cellIdentityLte.ci == Int.MAX_VALUE) 0 else cellIdentityLte.ci
+            rsrp = cellSignalStrengthLte.rsrp
+            rsrq = cellSignalStrengthLte.rsrq
+        }
+    }
+
+    private fun extractCellTowerInfoFromGsm(cellInfo: CellInfoGsm): CellTowerInfo {
+        val cellIdentityGsm = cellInfo.cellIdentity
+        val cellSignalStrengthGsm = cellInfo.cellSignalStrength
+
+        return CellTowerInfo().apply {
+            operator = cellIdentityGsm.operatorAlphaLong.toString()
+            mcc = cellIdentityGsm.mccString ?: ""
+            mnc = cellIdentityGsm.mncString ?: ""
+            lac = if (cellIdentityGsm.lac == Int.MAX_VALUE) 0 else cellIdentityGsm.lac
+            cid = if (cellIdentityGsm.cid == Int.MAX_VALUE) 0 else cellIdentityGsm.cid
+            rsrp = cellSignalStrengthGsm.dbm
+            rsrq = cellSignalStrengthGsm.asuLevel
+        }
     }
 }
